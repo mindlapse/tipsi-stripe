9b64b8d2bd68e1e4543d0a2d446d82bfdf7e0ea6
Dave MacDonald <mindlapse@gmail.com>
2019-07-09 17:34:09 -0400

    Adding extra logs to before-ci.sh

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -2,12 +2,18 @@
 
 case "${TRAVIS_OS_NAME}" in
   linux)
-    echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a --skin WVGA800
+    echo "Creating AVD"
+    android create avd --force -n test -t android-21 --abi armeabi-v7a --skin WVGA800
+    echo "Starting emulator as a background process"
     emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window &
+    echo "Waiting for emulator"
     android-wait-for-emulator
+    echo "Sleeping for 60"
     sleep 60
+    echo "Unlocking the device screen"
     adb shell input keyevent 82 &
   ;;
 esac
 
+echo "Starting appium"
 example_tmp/node_modules/.bin/appium --session-override > appium.out &
