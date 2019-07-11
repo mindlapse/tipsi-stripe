bafa32fb497b8297c69525b21920ef78eb1e925c
Dave MacDonald <mindlapse@gmail.com>
2019-07-11 17:11:23 -0400

    Trying alternate config
    Inspiration from:
    https://travis-ci.org/mmcc007/test_emulators/jobs/495210065/config

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -4,25 +4,49 @@ echo ".travis/before-ci.sh starting"
 
 case "${TRAVIS_OS_NAME}" in
   linux)
-    echo "Listing devices"
-    android list device
-    echo "Listing targets"
-    android list targets
-    echo "Listing avds"
-    android list avd
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    sleep 3 # give xvfb some time to start
+
     echo "Listing available component"
     sdkmanager --list
-    echo "Creating AVD"
-    echo 'no' | android create avd --abi "google_apis/armeabi-v7a" --name test --force -t android-25
-    echo "Starting emulator as a background process"
-    emulator -avd test -scale 96dpi -dpi-device 160 -no-window &
-    echo "Waiting for emulator"
+    ANDROID_TOOLS=4333796 # android-28
+    export ANDROID_HOME=~/android-sdk
+    wget -q "https://dl.google.com/android/repository/sdk-tools-linux-$ANDROID_TOOLS.zip" -O android-sdk-tools.zip
+    # Install android tools
+    unzip -q android-sdk-tools.zip -d ${ANDROID_HOME}
+    rm android-sdk-tools.zip
+    PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools
+    # Silence warning.
+    mkdir -p ~/.android
+    touch ~/.android/repositories.cfg
+    # Accept licenses before installing components, no need to echo y for each component
+    yes | sdkmanager --licenses
+
+    # Platform tools
+    sdkmanager "emulator" "tools" "platform-tools" > /dev/null
+    sdkmanager --list | head -15
+    # install older build tools (for emulator)
+    sdkmanager "build-tools;25.0.2" "platforms;android-25" > /dev/null
+    Create and start emulator.
+    sdkmanager "system-images;android-$SYS;$ABI" > /dev/null
+    sdkmanager --list | head -15
+    echo no | avdmanager create avd -n test -k "system-images;android-$SYS;$ABI"
+    # fix timezone warning on osx
+    if [[ "${SYS}${ABI}" == "25google_apis;armeabi-v7a" || "${SYS}${ABI}" == "24google_apis;armeabi-v7a" ]]; then
+      EMU_PARAMS="-no-window -gpu swiftshader"
+    else
+      EMU_PARAMS="-no-boot-anim -gpu off"
+    fi
+    # use the absolute emulator path in case older version installed (on default path)
+    $ANDROID_HOME/emulator/emulator -avd test -no-audio $EMU_PARAMS &
+
     android-wait-for-emulator
     echo "Sleeping for 60"
     sleep 60
     echo "Unlocking the device screen"
     adb shell input keyevent 82 &
-  ;;
+    ;;
 esac
 
 echo "Starting appium"
