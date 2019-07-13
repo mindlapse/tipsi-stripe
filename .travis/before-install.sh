1929453e49e7bdd5cff6508cbd809c99c41354f6
Dave MacDonald <mindlapse@gmail.com>
2019-07-13 15:31:16 -0400

    Fixed ANDROID_SDK_ROOT

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -59,14 +59,16 @@ case "${TRAVIS_OS_NAME}" in
     travis_wait pod repo update --silent
   ;;
   linux)
-    sdkmanager --list | head -15                                  # Print out package list for debug purposes
+    echo ANDROID_HOME=ANDROID_HOME
+    echo ANDROID_SDK_ROOT=$ANDROID_SDK_ROOT
+    sdkmanager --list | head -30                                              # Print out package list for debug purposes
 #    sdkmanager --uninstall "extras;google;google_play_services"
-    yes | sdkmanager "tools"                                      # Update tools
-    yes | sdkmanager "platforms;android-${EMULATOR_API_LEVEL}"    # Android platform required by emulator
-    yes | sdkmanager "platforms;android-${COMPILE_API_LEVEL}"     # Android platform required by compiler
-    yes | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" # Android build tools
-    yes | sdkmanager "${EMULATOR}"                                # Install emulator system image
-    sdkmanager --list | head -15                                  # Print out package list for debug purposes
+    yes | sdkmanager "tools"                                      > /dev/null  # Update tools
+    yes | sdkmanager "platforms;android-${EMULATOR_API_LEVEL}"    > /dev/null  # Android platform required by emulator
+    yes | sdkmanager "platforms;android-${COMPILE_API_LEVEL}"     > /dev/null  # Android platform required by compiler
+    yes | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" > /dev/null  # Android build tools
+    yes | sdkmanager "${EMULATOR}"                                > /dev/null  # Install emulator system image
+    sdkmanager --list | head -30                                              # Print out package list for debug purposes
   ;;
 esac
 
