db691b530c625aec9a92a85635a2a85bf25a9169
Dave MacDonald <mindlapse@gmail.com>
2019-07-13 21:11:40 -0400

    Added skin

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -37,11 +37,12 @@ case "${TRAVIS_OS_NAME}" in
     export PATH="~/android-sdk/emulator:${PATH}"
 
     echo "### Using avdmanager $(which avdmanager)"
-    echo no | avdmanager create avd --force -n ${EMULATOR_NAME} -k "${EMULATOR}"
+    echo no | avdmanager create avd --force -n ${EMULATOR_NAME} -k "${EMULATOR}" -d "4in WVGA (Nexus S)"
 
     echo "### Using emulator $(dirname $(which emulator))"
     # Run emulator in a subshell, this seems to solve the travis QT issue
-    ( cd "$(dirname $(which emulator))" && ./emulator -avd ${EMULATOR_NAME} -verbose -show-kernel -selinux permissive -no-audio -no-window -no-boot-anim -wipe-data & )
+    ( cd "$(dirname $(which emulator))" && ./emulator -avd ${EMULATOR_NAME} -verbose -show-kernel -selinux permissive -no-audio -no-window -wipe-data > /dev/null & )
+#    ( cd "$(dirname $(which emulator))" && ./emulator -avd ${EMULATOR_NAME} -verbose -show-kernel -selinux permissive -no-audio -no-window -no-boot-anim -wipe-data & )
     android-wait-for-emulator
     adb shell settings put global window_animation_scale 0 &
     adb shell settings put global transition_animation_scale 0 &
