00b33bde3656daedaa656c668366815c25152358
Dave MacDonald <mindlapse@gmail.com>
2019-07-14 00:28:11 -0400

    Upped timeout

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -7,7 +7,7 @@ android-wait-for-emulator() {
 
   bootanim=""
   failcounter=0
-  timeout_in_sec=360
+  timeout_in_sec=900
 
   until [[ "$bootanim" =~ "stopped" ]]; do
     bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
