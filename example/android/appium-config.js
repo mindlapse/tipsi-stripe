79e93fe8c12733abed997b00b4ab7a305eff5eec
Dave MacDonald <mindlapse@gmail.com>
2019-07-15 22:50:53 -0400

    Update appium-config.js

--- example/android/appium-config.js
+++ example/android/appium-config.js
@@ -1,6 +1,8 @@
 {
-  newCommandTimeout : "300000",
-  androidInstallTimeout : "300000",
-  automationName : "uiautomator2",
-  uiautomator2ServerInstallTimeout : "300000"
+  desiredCapabilities : {
+    newCommandTimeout : "300000",
+    androidInstallTimeout : "300000",
+    automationName : "uiautomator2",
+    uiautomator2ServerInstallTimeout : "300000"
+  }
 }
