8a5699089f5a95d9dc4d0223e18e245b88a3045c
Dave MacDonald <mindlapse@gmail.com>
2019-07-20 12:03:28 -0400

    Debug

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -11,12 +11,15 @@ case "${TRAVIS_OS_NAME}" in
   linux)
     npm run build:android
 
+    echo "Sending curl request to localhost:4723/wd/hub/status"
+    curl localhost:4723/wd/hub/status
     echo "Appium version"
     node_modules/.bin/appium --version
     echo "------------"
     ps aux|grep appium
     npm run test:android
     ps aux|grep appium
+    curl localhost:4723/wd/hub/status
     cat ../appium.out
   ;;
 esac
