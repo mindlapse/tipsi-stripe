c02848083125de802dbfc7c6d10a710dd7a1de1b
Dave MacDonald <mindlapse@gmail.com>
2019-07-20 16:10:03 -0400

    API 21

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -4,13 +4,13 @@ case "${TRAVIS_OS_NAME}" in
   linux)
 
     # Update sdk platform for emulator
-    echo "y" | android update sdk -a --no-ui --filter "android-24"
+    echo "y" | android update sdk -a --no-ui --filter "android-21"
 
     # Update the emulator sys image
-    echo "y" | android update sdk -a --no-ui --filter "sys-img-armeabi-v7a-android-24"
+    echo "y" | android update sdk -a --no-ui --filter "sys-img-armeabi-v7a-android-21"
 
     # Creating and waiting for emulator to run tests
-    echo no | android create avd --force -n test -t "android-24" --abi "armeabi-v7a" -c 100M
+    echo no | android create avd --force -n test -t "android-21" --abi "armeabi-v7a" -c 100M
     QEMU_AUDIO_DRV=none emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window &
     android-wait-for-emulator
     adb shell input keyevent 82 &
