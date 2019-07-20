12190936f3354b7848a2dd01db6a8307fda80726
Dave MacDonald <mindlapse@gmail.com>
2019-07-20 19:59:47 -0400

    Update before-ci.sh

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -9,9 +9,12 @@ case "${TRAVIS_OS_NAME}" in
     # Update the emulator sys image
     echo "y" | android update sdk -a --no-ui --filter "sys-img-armeabi-v7a-android-24"
 
-    # Creating and waiting for emulator to run tests
-    echo no | android create avd --force -n test -t "android-24" --abi "armeabi-v7a" -c 100M
+    echo "Cretaing AVD"
+    echo no | android create avd --force -n test -t android-24 --abi armeabi-v7a --skin WVGA800
+
+    echo "Starting emulator"
     QEMU_AUDIO_DRV=none emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window &
+
     android-wait-for-emulator
     adb shell input keyevent 82 &
 
