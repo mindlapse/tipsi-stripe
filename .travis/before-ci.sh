9bcbf1134f9f785fbb5e66fbababd88c4b67de99
Dave MacDonald <mindlapse@gmail.com>
2019-07-20 19:11:29 -0400

    Update before-ci.sh

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -18,7 +18,6 @@ case "${TRAVIS_OS_NAME}" in
     echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
     sdkmanager --list | head -30
-#    adb shell sh -c 'cmd package list packages -f'
   ;;
 esac
 
